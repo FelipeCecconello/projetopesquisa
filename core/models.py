eea1c331e01a09575983cae9d33297437b689e00
Felipe Fontana <felipec.fontana@gmail.com>
2023-05-16 14:35:14 -0400

    adicionando funcao str nas models

--- core/models.py
+++ core/models.py
@@ -7,6 +7,9 @@ class Professor(models.Model):
     nome = models.CharField(max_length=100)
     # Outros campos para o professor
 
+    def __str__(self):
+        return self.nome
+    
 class ProjetoPesquisa(models.Model):
     professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=100)
@@ -14,19 +17,30 @@ class ProjetoPesquisa(models.Model):
     inicio = models.DateField()
     fim = models.DateField()
 
+    def __str__(self):
+        return self.titulo
+    
 class Atividade(models.Model):
     projeto = models.ForeignKey(ProjetoPesquisa, on_delete=models.CASCADE)
     descricao = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.descricao
+    
 class Relatorio(models.Model):
     projeto = models.ForeignKey(ProjetoPesquisa, on_delete=models.CASCADE)
     descricao = models.CharField(max_length=100)
     titulo = models.CharField(max_length=100)
     relatorio_pdf = models.FileField(upload_to='relatorios/')
 
+    def __str__(self):
+        return self.titulo
+
 class Aluno(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     projeto = models.ForeignKey(ProjetoPesquisa, on_delete=models.CASCADE)
     cpf = models.CharField(max_length=11)
     nome = models.CharField(max_length=100)
-    # Outros campos para o aluno
+    
+    def __str__(self):
+        return self.nome
